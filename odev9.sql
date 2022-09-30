1db3f9daf30a67ce0d5463f3b796e31d412758c0
MadMan536 <95918698+MadMan536@users.noreply.github.com>
2022-09-30 11:43:01 +0300

    odev9.sql

---
+++ odev9.sql
@@ -0,0 +1,10 @@
+--Select country,city from city
+--INNER JOIN country ON city.country_id=country.country_id;
+
+--select payment_id, first_name, last_name from customer 
+--INNER JOIN PAYMENT ON 
+--PAYMENT.customer_id = customer.customer_id;
+
+--SELECT rental_id, first_name, last_name from customer 
+--inner join rental on 
+--rental.customer_id = customer.customer_id;
